64de44f92c9aaed859cd6d5e6a9830d8a8eaffe2
colinale <colinale@buffalo.edu>
2022-06-17 10:22:00 -0400

    creating index js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-// TODO: Include packages needed for this application
+// Declaring the dependencies and variables
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateReadme = require("./utils/generateReadme")
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+//Prompt the user questions to populate the README.md
+function promptUser(){
