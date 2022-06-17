a753503fce92d07e19aa9f3f5d68645fa4c4d57a
colinale <colinale@buffalo.edu>
2022-06-17 10:20:13 -0400

    finished generate marketdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -23,3 +23,26 @@ function generateReadme(answers) {
 ## Usage
 💻 ${answers.usage}
 
+## License
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+<br />
+This application is covered by the ${answers.license} license. 
+
+## Contributing
+👪 ${answers.contributing}
+
+## Tests
+✏️ ${answers.tests}
+
+## Questions
+✋ ${answers.questions}<br />
+<br />
+:octocat: Find me on GitHub: [${answers.username}](https://github.com/${answers.username})<br />
+<br />
+✉️ Email me with any questions: ${answers.email}<br /><br />
+
+_This README was generated with ❤️ by [README-generator](https://github.com/jpd61/README-generator) 🔥🔥🔥_
+  `;
+}
+
+module.exports = generateReadme;
