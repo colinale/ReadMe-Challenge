865b6123b83797e88c55bde66cb19bf69cff9b01
colinale <colinale@buffalo.edu>
2022-06-17 10:17:10 -0400

    table of contents

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,25 @@
+// generateReadme function populating the README.md
+function generateReadme(answers) {
+  return `
+<h1 align="center">${answers.projectTitle} 👋</h1>
+
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)<br />
+
+## Description
+🔍 ${answers.description}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+💾 ${answers.installation}
+
+## Usage
+💻 ${answers.usage}
+
